bc32cdc02f17299c354ef197eb23ae3fbfc4c85f
Mariana Galvão <81964220+Mariana-gs@users.noreply.github.com>
2022-10-13 12:45:01 -0300

    update

--- CamaraConservacao/main/main.c
+++ CamaraConservacao/main/main.c
@@ -6,10 +6,10 @@
 
 // Definições pré main(): pinos, variáveis, etc
 
-#define pinoLI1 15
+//#define pinoLI1 15
 #define pinoLI2 2
 #define pinoLI3 4
-#define pinoLI4 16
+//#define pinoLI4 16
 #define pinoSensorRS 17
 
 #define pinoPeltier 5
@@ -22,15 +22,15 @@ void porta(void *pvParameter)
   {
     bool sensorPorta = gpio_get_level(pinoSensorRS);
     if(sensorPorta == 0){
-    gpio_set_level(pinoLI1, 1);
+    //gpio_set_level(pinoLI1, 1);
     gpio_set_level(pinoLI2, 1);
     gpio_set_level(pinoLI3, 1);
-    gpio_set_level(pinoLI4, 1);
+    //gpio_set_level(pinoLI4, 1);
     }else{
-    gpio_set_level(pinoLI1, 0);
+    //gpio_set_level(pinoLI1, 0);
     gpio_set_level(pinoLI2, 0);
     gpio_set_level(pinoLI3, 0);
-    gpio_set_level(pinoLI4, 0);
+    //gpio_set_level(pinoLI4, 0);
     }
   }
 }
@@ -38,14 +38,14 @@ void porta(void *pvParameter)
 void app_main(void){  // Main // equivalente ao setup() do Arduino
 
 //Pinos Iluminação Interna
-gpio_pad_select_gpio(pinoLI1);
-gpio_set_direction(pinoLI1, GPIO_MODE_OUTPUT);
+//gpio_pad_select_gpio(pinoLI1);
+//gpio_set_direction(pinoLI1, GPIO_MODE_OUTPUT);
 gpio_pad_select_gpio(pinoLI2);
 gpio_set_direction(pinoLI2, GPIO_MODE_OUTPUT);
 gpio_pad_select_gpio(pinoLI3);
 gpio_set_direction(pinoLI3, GPIO_MODE_OUTPUT);
-gpio_pad_select_gpio(pinoLI4);
-gpio_set_direction(pinoLI4, GPIO_MODE_OUTPUT);
+//gpio_pad_select_gpio(pinoLI4);
+//gpio_set_direction(pinoLI4, GPIO_MODE_OUTPUT);
 
 //Pino Reed Switch
 gpio_pad_select_gpio(pinoSensorRS);
@@ -68,8 +68,7 @@ gpio_set_level(pinoPeltier, 0);
     while (1) { //equivalente ao loop() do Arduino   
 
 
-    
-
+  
     }// Fim do Loop
 
 }// Fim do Main
