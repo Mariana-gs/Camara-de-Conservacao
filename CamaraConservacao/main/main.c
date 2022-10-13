6db2c08139425601768b53a5b3ff90f8b24e9450
Mariana Galvão <81964220+Mariana-gs@users.noreply.github.com>
2022-10-13 14:14:45 -0300

    Sensor Temperatura

--- CamaraConservacao/main/main.c
+++ CamaraConservacao/main/main.c
@@ -3,17 +3,22 @@
 #include "sdkconfig.h"  // carrega informações do build
 #include "driver/gpio.h"
 #include "esp_rom_gpio.h"
+#include <stdio.h>
+#include "driver/adc.h"
+
+
 
 // Definições pré main(): pinos, variáveis, etc
 
-//#define pinoLI1 15
+
 #define pinoLI2 2
 #define pinoLI3 4
-//#define pinoLI4 16
 #define pinoSensorRS 17
 
 #define pinoPeltier 5
 #define pinoCoolers 18
+
+#define TMP ADC1_CHANNEL_3 //39
 
 //
 void porta(void *pvParameter)
@@ -37,6 +42,10 @@ void porta(void *pvParameter)
 
 void app_main(void){  // Main // equivalente ao setup() do Arduino
 
+//Configura conversor AD
+adc1_config_width(ADC_WIDTH_BIT_10);
+adc1_config_channel_atten(TMP, ADC_ATTEN_DB_0);
+
 //Pinos Iluminação Interna
 //gpio_pad_select_gpio(pinoLI1);
 //gpio_set_direction(pinoLI1, GPIO_MODE_OUTPUT);
@@ -46,6 +55,9 @@ gpio_pad_select_gpio(pinoLI3);
 gpio_set_direction(pinoLI3, GPIO_MODE_OUTPUT);
 //gpio_pad_select_gpio(pinoLI4);
 //gpio_set_direction(pinoLI4, GPIO_MODE_OUTPUT);
+
+
+
 
 //Pino Reed Switch
 gpio_pad_select_gpio(pinoSensorRS);
@@ -62,13 +74,19 @@ gpio_set_direction(pinoCoolers, GPIO_MODE_OUTPUT);
 xTaskCreate(porta,"porta",configMINIMAL_STACK_SIZE,NULL,1,NULL);
 
 gpio_set_level(pinoCoolers, 1); 
-gpio_set_level(pinoPeltier, 0); 
+gpio_set_level(pinoPeltier, 1); 
 
-
+    
     while (1) { //equivalente ao loop() do Arduino   
 
+    int valorSensor = adc1_get_raw(TMP);
+    int temperaturaC = (valorSensor-500)/10;         
+       
+    printf("Temperatura: %d C\n", temperaturaC);
 
-  
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    fflush(stdout);
+    
     }// Fim do Loop
 
 }// Fim do Main
