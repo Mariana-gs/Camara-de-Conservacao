850ffe30de57853845c938f262b07ad9dcb8d2fc
Mariana Galvão <81964220+Mariana-gs@users.noreply.github.com>
2022-10-19 13:39:37 -0300

    Código primeira parte

--- CamaraConservacao/main/main.c
+++ CamaraConservacao/main/main.c
@@ -1,59 +1,42 @@
-#include <freertos/FreeRTOS.h>// FreeRTOS para usar delay
+#include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include "sdkconfig.h"  // carrega informações do build
+#include "sdkconfig.h" 
 #include "driver/gpio.h"
 #include "esp_rom_gpio.h"
 #include <stdio.h>
 #include "driver/adc.h"
 
-
-// Definições pré main(): pinos, variáveis, etc
-
-#define pinoLI2 2
-#define pinoLI3 4
+// Definições pré main():
+#define pinoLI1 2 //LED 1
+#define pinoLI2 4 //LED 2
 #define pinoSensorRS 17
-
 #define pinoPeltier 5
 #define pinoCoolers 18
 
-#define TMP ADC1_CHANNEL_0 //36
-
-//
+//Iluminação
 void porta(void *pvParameter)
 {
   while(1)
   {
     bool sensorPorta = gpio_get_level(pinoSensorRS);
     if(sensorPorta == 0){
-    //gpio_set_level(pinoLI1, 1);
+    gpio_set_level(pinoLI1, 1);
     gpio_set_level(pinoLI2, 1);
-    gpio_set_level(pinoLI3, 1);
-    //gpio_set_level(pinoLI4, 1);
+  
     }else{
-    //gpio_set_level(pinoLI1, 0);
+    gpio_set_level(pinoLI1, 0);
     gpio_set_level(pinoLI2, 0);
-    gpio_set_level(pinoLI3, 0);
-    //gpio_set_level(pinoLI4, 0);
     }
   }
 }
 
-void app_main(void){  // Main // equivalente ao setup() do Arduino
-
-//Configura conversor AD
-adc1_config_width(ADC_WIDTH_BIT_10);
-adc1_config_channel_atten(TMP, ADC_ATTEN_DB_0);
+void app_main(void){ 
 
 //Pinos Iluminação Interna
-//gpio_pad_select_gpio(pinoLI1);
-//gpio_set_direction(pinoLI1, GPIO_MODE_OUTPUT);
+gpio_pad_select_gpio(pinoLI1);
+gpio_set_direction(pinoLI1, GPIO_MODE_OUTPUT);
 gpio_pad_select_gpio(pinoLI2);
 gpio_set_direction(pinoLI2, GPIO_MODE_OUTPUT);
-gpio_pad_select_gpio(pinoLI3);
-gpio_set_direction(pinoLI3, GPIO_MODE_OUTPUT);
-//gpio_pad_select_gpio(pinoLI4);
-//gpio_set_direction(pinoLI4, GPIO_MODE_OUTPUT);
-
 
 //Pino Reed Switch
 gpio_pad_select_gpio(pinoSensorRS);
@@ -65,31 +48,12 @@ gpio_set_direction(pinoPeltier, GPIO_MODE_OUTPUT);
 gpio_pad_select_gpio(pinoCoolers);
 gpio_set_direction(pinoCoolers, GPIO_MODE_OUTPUT);
 
-
-//Taks Porta
-xTaskCreate(porta,"porta",configMINIMAL_STACK_SIZE,NULL,1,NULL);
-
+//Ligar Geladeira
 gpio_set_level(pinoCoolers, 1); 
 gpio_set_level(pinoPeltier, 1); 
 
+//Task Porta
+xTaskCreate(porta,"porta",configMINIMAL_STACK_SIZE,NULL,1,NULL);
     
-    while (1) { //equivalente ao loop() do Arduino   
-
-    int valorSensor = adc1_get_raw(TMP);
-    int vout;
-    //int temperaturaC = (valorSensor-500)/10; 
-    //int temperaturaC = (valorSensor*0.4915)- 50.771;
-      
-    printf("valorSensor: %d\n", valorSensor);
-    vout = valorSensor * 950/1024;
-    printf("vout: %d\n", vout);
-
-    int temperaturaC = (vout - 500)/10;
-    printf("Temperatura: %d C\n", temperaturaC);
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
-    fflush(stdout);
-    
-    }// Fim do Loop
-
 }// Fim do Main
 
